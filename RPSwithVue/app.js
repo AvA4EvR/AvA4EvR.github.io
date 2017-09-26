268a7cdebb1297b4d90d49ad3e09cb4c6ae60b19
AvA4EvR <31218871+AvA4EvR@users.noreply.github.com>
2017-09-26 16:57:53 -0600

    Selection for computer, randomly generated

--- RPSwithVue/app.js
+++ RPSwithVue/app.js
@@ -4,11 +4,19 @@ new Vue({
       paperButton: false,
 	  rockButton: false,
 	  scissorsButton: false,
+      computerRock: false,
+      computerPaper: false,
+      computerScissors: false,
 	  playerScore: 0,
 	  computerScore: 0,
       buttonsVisible: true,
       buttonsStart: true,
-      buttonClick: true
+      buttonClick: true,
+      messages: [
+          { message: 'Computer chose'},
+          { message: 'Beats'},
+          { message: ' Wins'},
+      ]
 
   },
   
@@ -19,11 +27,23 @@ new Vue({
       }
     },
 
+    increaseComputerBar: function(){
+        return {
+        width: this.computerScore + '%'
+        }
+      },
+
   },
   
   methods: {
       increasePlayerScore: function(){
         this.playerScore++
+      },
+
+      reset: function() {
+        this.computerRock = false,
+        this.computerPaper = false,
+        this.computerScissors = false
       },
 	  
 	  increaseComputerScore: function() {
@@ -31,8 +51,23 @@ new Vue({
 	  },
 
 	  computerSelection: function(){
-          return Math.floor(Math.random() * 3) + 1
-      }
+          var selection = Math.floor(Math.random() * 3) + 1
+          if (selection === 1){
+              this.computerScissors = false
+              this.computerPaper = false
+              this.computerRock = true
+          }
+          else if (selection === 2){
+              this.computerRock = false
+              this.computerScissors = false
+              this.computerPaper = true
+          }
+          else
+              this.computerRock = false
+              this.computerPaper = false
+              this.computerScissors = true
+      },
+
   },
   
 });
