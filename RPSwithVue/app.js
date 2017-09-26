05c036184c7d5534f383be194a0de40546448447
AvA4EvR <31218871+AvA4EvR@users.noreply.github.com>
2017-09-26 17:33:37 -0600

    Setting up checkTurn function to check player and computer turns

--- RPSwithVue/app.js
+++ RPSwithVue/app.js
@@ -1,9 +1,9 @@
 new Vue({
   el: '#app',
   data: {
-      paperButton: false,
-	  rockButton: false,
-	  scissorsButton: false,
+      playerPaper: false,
+	  playerRock: false,
+	  playerScissors: false,
       computerRock: false,
       computerPaper: false,
       computerScissors: false,
@@ -40,15 +40,17 @@ new Vue({
         this.playerScore++
       },
 
-      reset: function() {
-        this.computerRock = false,
-        this.computerPaper = false,
-        this.computerScissors = false
-      },
-	  
 	  increaseComputerScore: function() {
 		this.computerScore++
 	  },
+
+      reset: function() {
+          this.computerRock = false
+          this.computerPaper = false
+          this.computerScissors = false
+          this.playerScore = 0
+          this.computerScore = 0
+      },
 
 	  computerSelection: function(){
           var selection = Math.floor(Math.random() * 3) + 1
@@ -66,6 +68,60 @@ new Vue({
               this.computerRock = false
               this.computerPaper = false
               this.computerScissors = true
+      },
+
+      playerSelection: function(select) {
+          if (select === 1){
+              this.playerScissors = false
+              this.playerPaper = false
+              this.playerRock = true
+          }
+          else if (select === 2){
+              this.playerRock = false
+              this.playerScissors = false
+              this.playerPaper = true
+          }
+          else{
+              this.playerRock = false
+              this.playerPaper= false
+              this.playerScissors = true
+          }
+      },
+
+      checkTurn: function(){
+	    if(this.playerRock === true){
+            if(this.computerRock === true){
+
+            }
+            else if(this.computerPaper === true){
+
+            }
+            else {
+
+            }
+        }
+        else if (this.playerPaper === true){
+            if(this.computerRock === true){
+
+            }
+            else if (this.computerPaper === true){
+
+            }
+            else {
+
+            }
+        }
+        else {
+            if (this.computerRock === true){
+
+            }
+            else if (this.computerPaper === true){
+
+            }
+            else {
+
+            }
+        }
       },
 
   },
